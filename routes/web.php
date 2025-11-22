45d8ce754285679b161c7178769c75990b176568
jelita <jelita.com>
2025-11-22 14:48:16 +0700

    home

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', [LoginController::class, 'show'])->name('login');
+Route::post('/login', [LoginController::class, 'submit'])->name('login.submit');
+
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
